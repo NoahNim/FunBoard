d6011202cd69f0c57a30bd4b02e3c0ac2d3164d2
noahnim <noahmedoff@gmail.com>
2023-01-24 02:14:38 -0800

    build root reducer

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,11 +1,25 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action, applyMiddleware, compose } from '@reduxjs/toolkit';
+
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+
   },
 });
+
+let enhancer;
+
+if (process.env.NODE_ENV !== 'production') {
+  const logger = require('redux-logger').default;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(logger));
+}
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
