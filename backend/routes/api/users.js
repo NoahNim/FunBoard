2930d3fdd964cbd31633665f3c267d53fd4a6e6e
noahnim <noahmedoff@gmail.com>
2023-01-24 00:26:36 -0800

    build session routes

---
+++ backend/routes/api/users.js
@@ -0,0 +1,57 @@
+
+const express = require("express");
+const { check } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.')
+    .isLength({ max: 100 })  
+  ,
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4, max: 30 })
+    .withMessage('Please provide an unused username with at least 4 characters but less than 30 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.')
+    .exists({ checkFalsy: true })
+    .withMessage("please input a username"),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  check('fullName')
+    .exists({ checkFalsy: true })
+    .withMessage("Please put in a first name.")
+    .isLength({  max: 256 })
+    .withMessage("Name can not be more than 256 characters"),
+  handleValidationErrors,
+];
+
+// Sign up
+router.post(
+  '',
+  validateSignup,
+  asyncHandler(async (req, res) => {
+    const { username, fullName, email, password, biography, profilePhoto } = req.body;
+    const user = await User.signup({ username, fullName, email, password, biography, profilePhoto });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  }),
+);
+
+module.exports = router;
