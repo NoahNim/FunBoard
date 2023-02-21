03cf4a7af6e2b5841b07f57e933f5bdc6ad56913
noahnim <noahmedoff@gmail.com>
2023-02-21 04:50:13 -0800

    add admin stickied messages

--- frontend/src/components/Messages/Messages.tsx
+++ frontend/src/components/Messages/Messages.tsx
@@ -19,6 +19,36 @@ export const Messages = () => {
             {!sessionUser ? <></> : <CreateMessage refetch={refetch} sessionUser={sessionUser} />}
             <div className="messages-list">
                 {messagesList?.map((message: Message, index: number) => {
+                    if (message.userId === 4) {
+                        return (
+                            <div className="box">
+                                <div className="box-container">
+                                    <div className="box-message">
+                                        <div className="box-message-contents">
+                                            <img className="post-images" src={`${window.location.href}${message?.photo}`}></img>
+                                            <ul key={message.id}>
+                                                {sessionUser?.id === message.userId || sessionUser?.username === "noah" ?
+                                                    <>
+                                                        <EditMessage title={message.title} message={message.message} id={message.id} sessionUser={sessionUser} refetch={refetch} />
+                                                        <DeleteMessage id={message.id} refetch={refetch} sessionUser={sessionUser} />
+                                                    </> : <></>}
+                                                <li key={index}>{message.title}</li>
+                                                <li key={`${message.message}${Math.random()}`}>
+                                                    <p>{message.message}</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <SingleMessage id={message.id} index={index} title={message?.title} message={message?.message} photo={message?.photo} />
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
+                {messagesList?.map((message: Message, index: number) => {
                     return (
                         <div className="box">
                             <div className="box-container">
