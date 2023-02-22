84556f0ccf5e7109badc8a405abf73684ecc1536
noahnim <noahmedoff@gmail.com>
2023-02-22 13:09:26 -0800

    build out new menu drawer css

--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,25 +1,59 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { MenuDrawer } from "./MenuDrawer";
 import "./menu.css"
+import {
+    Button,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+} from '@chakra-ui/react'
+import { useAppSelector } from "../../redux/app/store";
+import { LoginForm } from "./auth/loginForm";
+import { LogoutButton } from "./auth/logoutButton";
+import { SignupForm } from "./auth/signupForm";
+import { UserInfo } from "./auth/UserInfo";
 
 export const Menu = () => {
-    const [drawerOpen, setDrawerOpen] = useState(false);
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = useRef<any | undefined | null>()
 
-    const toggleDrawerHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-
-
-        if (!drawerOpen) {
-            setDrawerOpen(true);
-        } else {
-            setDrawerOpen(false);
-        }
-    }
 
     return (
-        <div>
-            <button onClick={toggleDrawerHandler} className="hamburger">Menu</button>
-            <MenuDrawer show={drawerOpen} toggle={toggleDrawerHandler} />
-        </div>
+        <>
+            <Button ref={btnRef} backgroundColor={'#CFD2CD'} onClick={onOpen}>
+                Open
+            </Button>
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerContent>
+                    <DrawerCloseButton />
+
+
+                    <DrawerBody>
+
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                        <Button mr={3} backgroundColor={"#e69797"} onClick={onClose}>
+                            Close
+                        </Button>
+                    </DrawerFooter>
+                </DrawerContent>
+
+
+
+
+            </Drawer>
+
+        </>
     )
 }
