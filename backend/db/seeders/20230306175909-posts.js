0547bc326b415032e2651fbd33e88d7c5176df5f
noahnim <noahmedoff@gmail.com>
2023-03-06 11:40:26 -0800

    fix up seed faker for msgs

--- backend/db/seeders/20230306175909-posts.js
+++ backend/db/seeders/20230306175909-posts.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { faker } = require('@faker-js/faker')
-
 let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
@@ -23,48 +21,48 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "The time has come",
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut metus a urna commodo fermentum. Morbi ut sagittis velit. Nulla fringilla nunc quis nibh placerat, vel pharetra velit commodo. Etiam vestibulum, urna sit amet molestie iaculis, arcu elit bibendum urna, vel semper nulla leo id turpis. Nunc interdum dolor eu ultrices tempus. Nullam euismod sem a ipsum posuere, vel vestibulum enim luctus. Nulla facilisi. Nullam eget bibendum velit. Donec id congue leo. Sed ac lectus vel tortor sodales interdum. Aliquam erat volutpat. Ut hendrerit eleifend tellus, eget sollicitudin ex. Vivamus venenatis eleifend enim, in feugiat metus consequat nec. Nulla facilisi.",
         photo: "https://i.imgur.com/xTCfqri.jpeg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "Really the time is here",
+        message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut metus a urna commodo fermentum. Morbi ut sagittis velit. Nulla fringilla nunc quis nibh placerat, vel pharetra velit commodo. Etiam vestibulum, urna sit amet molestie iaculis, arcu elit bibendum urna, vel semper nulla leo id turpis. Nunc interdum dolor eu ultrices tempus. Nullam euismod sem a ipsum posuere, vel vestibulum enim luctus. Nulla facilisi. Nullam eget bibendum velit. Donec id congue leo. Sed ac lectus vel tortor sodales interdum. Aliquam erat volutpat. Ut hendrerit eleifend tellus, eget sollicitudin ex. Vivamus venenatis eleifend enim, in feugiat metus consequat nec. Nulla facilisi.",
         photo: "https://i.imgur.com/K4i3rEZb.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "IT IS HERE",
+        message: "Time to cut the meat. Get on that grind. Explore the world! Image unrelated.",
         photo: "https://i.imgur.com/A95v0LUb.jpg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "We did it!",
+        message: "This might be a few months later but we did it.",
         photo: "https://i.imgur.com/RUteQr9.jpeg",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "What if...",
+        message: "What if this guy had been the Last Jedi?",
         photo: "https://i.imgur.com/5trmBJU.png",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
         userId: 1,
-        title: faker.lorem.words(),
-        message: faker.lorem.sentences(),
+        title: "This is not my car.",
+        message: "But it's a picture of someone else's car!",
         photo: "https://i.imgur.com/1GhPAk3.jpeg",
         createdAt: new Date(),
         updatedAt: new Date(),
